7ac2ee108d25fa0aeb5c130714a0b2e95e76a73e
Christin00 <Christin.brae@outlook.de>
2021-03-06 14:06:21 +0100

    Anforderung 102b

--- src/main/java/dhbw/fowler1/videostore/Customer.java
+++ src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -21,7 +21,7 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             // determine amount for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -48,23 +48,4 @@ public class Customer {
         return _name;
     }
 
-    private double amountFor(Rental aRental){
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
 }
